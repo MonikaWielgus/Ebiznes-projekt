f0beb92d6d2e4e98e719067433e92937de8b753e
monikawielgus <monika.wielgus1999@gmail.com>
2022-07-04 17:30:04 +0200

    Handles cors

--- bookshop/src/main/kotlin/com/example/Application.kt
+++ bookshop/src/main/kotlin/com/example/Application.kt
@@ -3,11 +3,18 @@ package com.example
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.cors.routing.*
+import io.ktor.http.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureRouting()
         configureSerialization()
         configureSecurity()
+        install(CORS) {
+            anyHost()
+            allowHeader(HttpHeaders.ContentType)
+        }
     }.start(wait = true)
 }
